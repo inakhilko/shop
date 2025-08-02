85c28151386654a99efb9d4a014f917451115d0f
Inna Khilko <innakhilko581@gamil.com>
2025-08-02 16:42:33 +0300

    feat: add swagger configuration

--- src/modules/products/dtos/products.dto.ts
+++ src/modules/products/dtos/products.dto.ts
@@ -6,23 +6,32 @@ import {
   IsString,
 } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
+  @ApiProperty({ example: 'Smartphone', description: 'Product name' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    example: 'Color blue, storage 128GB',
+    description: 'Product description',
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({ example: 799.99, description: 'Product price' })
   @IsNumber()
   @IsPositive()
   price: number;
 
+  @ApiProperty({ example: 1, description: 'Product category id' })
   @IsNumber()
   categoryId: number;
 
+  @ApiProperty({ example: 1, description: 'Product subcategory id' })
   @IsNumber()
   @IsOptional()
   subcategoryId?: number;
